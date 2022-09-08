7f6cceee189ea508a60e446896d521218b8d3589
澤西一樹 <sawanishiichiju@sawanishiichijunoMacBook-Air.local>
2022-09-08 23:31:27 +0900

    jQuery-Lesson5章

--- app.js
+++ app.js
@@ -1,3 +1,47 @@
-$(document).ready(function () {
-  $('body').html('<h1>Hello jQuery!!</h1>');
+// $(document).ready(function () {
+// // $( セレクタ ).メソッド ( パラメータ );
+//   $('body').html('<h1>Hello jQuery!!</h1>');
+// });
+
+// $(document).ready(function () {
+//   // jQueryプログラムの内容
+// });
+
+// $(function(){
+//   $('.box1').css({
+//     'background-color': '#0000FF',
+//     'height': '100px'
+//   });
+// });
+
+// //1つのCSSを記述する場合
+// $('セレクタ').css('background-color','#0000FF');
+
+// 今回のように複数のプロパティを指定するとき
+// $('セレクタ').css({
+//   'プロパティ1': '値1',
+//   'プロパティ2': '値2',
+
+//   :
+//   :
+
+//   'プロパティn': '値n'
+// });
+
+// $(function(){
+//   $('.box1').slideDown();
+// });
+
+// $(function(){
+//   $('.box1').slideUp();
+// });
+
+// $(function(){
+//   $('.box1').show();
+//   $('.box1').css({'background-color': '#0000FF'});
+// });
+
+$(function(){
+  $('.box1').hide();
 });
+
