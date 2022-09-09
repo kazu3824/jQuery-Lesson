a5e82bf72bfdfd4386815a97c4194dfdbe7138b6
澤西一樹 <sawanishiichiju@sawanishiichijunoMacBook-Air.local>
2022-09-09 20:14:51 +0900

    【thisとchildren】

--- app.js
+++ app.js
@@ -67,12 +67,44 @@
 //   イベント発生時に行われる処理
 // });
 
+// $(function(){
+//   $('.box1').on('click', function(){
+//     $('.box1').addClass('box1-ext');
+//   });
+//   $('.box1').mouseout(function(){
+//     $('.box1').removeClass('box1-ext');
+//   });
+// });
+
+// $(function(){
+//   $('.bg1').on(`click`, function(){
+//     $(`.bg1`).slideUp();
+//   });
+
+//   $('.bg2').on('click', function(){
+//     $('.bg2').slideUp();
+//   });
+
+//   $('.bg3').on('click', function(){
+//     $('.bg3').slideUp();
+//   });
+
+//   $('.bg4').on('click', function(){
+//     $('.bg4').slideUp();
+//   });
+// });
+
+
+// thisは、「クリックされた要素」を指しています。
+// $(function(){
+//   $('.box1').on('click', function(){
+//     $(this).slideUp();
+//   });
+// });
+
 $(function(){
-  $('.box1').on('click', function(){
-    $('.box1').addClass('box1-ext');
-  });
-  $('.box1').mouseout(function(){
-    $('.box1').removeClass('box1-ext');
+  $('button').on('click', function(){
+    $('ul').children().css('color','red');
   });
 });
 
